3726617e217a99f8adbd6d1a58093d36cf9d88a1
GemsGame <a.masanovets@gmail.com>
2019-06-10 02:50:02 +0300

    feat: add pagination

--- client/src/components/PagePagination/PagePagination.js
+++ client/src/components/PagePagination/PagePagination.js
@@ -1,16 +1,46 @@
-import React from 'react'
-import { Pagination } from 'semantic-ui-react'
+import React, { Component } from "react";
+import { Pagination } from "semantic-ui-react";
+import { connect } from "react-redux";
+import { chargersFetchData } from "../../actions/chargers";
 
-const PagePagination = () => (
-  <Pagination
-    boundaryRange={0}
-    defaultActivePage={1}
-    ellipsisItem={null}
-    firstItem={null}
-    lastItem={null}
-    siblingRange={1}
-    totalPages={10}
-  />
-)
+class PagePagination extends Component {
+  
+  componentWillMount() {
+    this.props.fetchData("/page=0&limit=30");
+  } 
 
-export default PagePagination;
+  activePage = page => {
+    this.props.fetchData(`/page=${page-1}&limit=30`);
+  };
+
+  render() {
+    return (
+      <Pagination
+        boundaryRange={5}
+        defaultActivePage={1}
+        ellipsisItem={null}
+        firstItem={null}
+        lastItem={null}
+        siblingRange={1}
+        totalPages={Infinity}
+        onPageChange={(e, props) => this.activePage(props.activePage)}
+      />
+    );
+  }
+}
+const mapStateToProps = state => {
+  return {
+    chargers: state.chargers
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    fetchData: url => dispatch(chargersFetchData(url))
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(PagePagination);
--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -5,37 +5,14 @@ import SearchForm from "../components/SearchForm";
 import Category from "../components/Category";
 import PagePagination from "../components/PagePagination";
 import { connect } from "react-redux";
-import { chargersFetchData } from "../actions/chargers";
-
 
 class App extends Component {
-  
-  componentWillMount() {
-    this.props.fetchData("/page=0&limit=10");
-  } 
-  
-
-  handleChange = event => {
-    this.setState({ search: event.target.value });
-  };
-
-  getResult = () => {
-    if (this.state.search !== "") {
-      const get = this.state.data.feed.entry.filter((item, index) =>
-        item.gsx$name.$t.match(this.state.search)
-      );
-      
-      this.setState({ result: get });
-    }
-  };
-
   render() {
- 
     return this.props.chargers != null ? (
       <div className="App">
         <Category />
-          <SearchForm/>
-          <RowList/>
+        <SearchForm />
+        <RowList />
         <PagePagination />
       </div>
     ) : null;
@@ -48,13 +25,4 @@ const mapStateToProps = state => {
   };
 };
 
-const mapDispatchToProps = dispatch => {
-  return {
-    fetchData: url => dispatch(chargersFetchData(url))
-  };
-};
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App);
+export default connect(mapStateToProps)(App);
